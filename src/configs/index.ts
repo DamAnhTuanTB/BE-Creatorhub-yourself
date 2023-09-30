3af34b987e2711593878180a63ac55951f6b5eb2
Anh Tuan (TuanDA) <tuanda@apero.vn>
2023-09-30 11:43:10 +0700

    Fix

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,12 +2,12 @@ export default () => ({
   PORT: process.env.PORT,
   MONGO_URL: process.env.MONGO_URI,
   AWS: {
-    PRIVATE_KEY: process.env.AWS_PRIVATE_KEY,
-    CDN: process.env.AWS_CDN,
-    ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    PRIVATE_KEY: process.env.AWS_SECRET_PRIVATE_KEY,
+    CDN: process.env.AWS_SECRET_CDN,
+    ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   },
   S3: {
-    BUCKET: process.env.S3_BUCKET,
-    REGION: process.env.S3_REGION,
+    BUCKET: process.env.S3_SECRET_BUCKET,
+    REGION: process.env.S3_SECRET_REGION,
   },
 });
