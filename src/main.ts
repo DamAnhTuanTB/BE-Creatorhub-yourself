18f65b95d1080e4f8a10cfe615215c85fb8a9509
Anh Tuan (TuanDA) <tuanda@apero.vn>
2023-10-01 16:05:58 +0700

    Add feature

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import configuration from './configs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  
   // app.setGlobalPrefix('nextapi', {
   //   exclude: ['/health'],
   // });
