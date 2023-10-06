19ca1734591bb689523ea2df796b26bb572db0d2
Anh Tuan (TuanDA) <tuanda@apero.vn>
2023-10-06 18:23:04 +0700

    add login with google

--- src/google/google.controller.ts
+++ src/google/google.controller.ts
@@ -4,8 +4,13 @@ import { Request, Response } from 'express';
 // import { AuthGuard } from '@nestjs/passport';
 import { GoogleOAuthGuard } from '../auth/guard/google-oauth.guard';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('google')
+@ApiTags('Google')
+@Controller({
+  path: 'google',
+  version: '1',
+})
 export class GoogleController {
   constructor(private readonly googleService: GoogleService) {}
 
